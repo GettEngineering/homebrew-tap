064bf100f9596d1f7433ffe0413c5c755fe0eb6d
freak4pc <freak4pc@gmail.com>
2020-07-04 18:04:01 +0300

    Prism 0.4.1

--- prism.rb
+++ prism.rb
@@ -1,9 +1,9 @@
 class Prism < Formula
     desc "Gett's Design System code generator. Use Zeplin Styleguides as your R&D's Single Source of Truth."
     homepage "https://prism.tools"
-    url "https://github.com/GettEngineering/Prism/archive/0.4.0.tar.gz"
-    version "0.4.0"
-    sha256 "4bb0f3f9f561b9a23083d8042de7e8dc2038c7e142bc64d3f42aff0870d75620"
+    url "https://github.com/GettEngineering/Prism/archive/0.4.1.tar.gz"
+    version "0.4.1"
+    sha256 "5664c63e697b1953de5ff4271fc84f94ca624a30a054c61fcd9837d4044b54ce"
 
     def install
         system "swift", "build",
