f6c2a5cb79fd4dd15c0a1f5a2c0ce683eb82a495
freak4pc <freak4pc@gmail.com>
2020-08-14 06:43:34 +0300

    Prism 0.5.0

--- prism.rb
+++ prism.rb
@@ -1,14 +1,13 @@
 class Prism < Formula
     desc "Gett's Design System code generator. Use Zeplin Styleguides as your R&D's Single Source of Truth."
     homepage "https://prism.tools"
-    url "https://github.com/GettEngineering/Prism/archive/0.4.1.tar.gz"
-    version "0.4.1"
-    sha256 "5664c63e697b1953de5ff4271fc84f94ca624a30a054c61fcd9837d4044b54ce"
+    url "https://github.com/GettEngineering/Prism/archive/0.5.0.tar.gz"
+    version "0.5.0"
+    sha256 "116ac1a7dd8b62804b8525615b8f71b1b4636c6bd7b78e1b418bc1b6f7aca982"
 
     def install
         system "swift", "build",
             "--configuration", "release",
-            "--enable-pubgrub-resolver",
             "--disable-sandbox"
         bin.install '.build/release/prism'
     end
